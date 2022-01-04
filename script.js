78050047763a1f80110f7875f7788076342b6952
SaroMusie <saronlebassi@gmail.com>
2022-01-04 14:26:06 +0000

    change

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML ="If you deposit "+principal+",\<br\>at an Interes rate of "+rate+"%\<br\>You will recieve an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML ="If you deposit "+principal+",\<br\>at an Interest rate of "+rate+"%\<br\>You will recieve an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 }
 function updateRate(){
     //This function updates the input type range 
